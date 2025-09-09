be37d11363a80133d44ba0337f7f4e9e65f06040
Pawan64bit <70272159+Pawan64bit@users.noreply.github.com>
2025-09-09 20:46:21 +0530

    Update DBConnection.java with properties

--- feed v2/litmus7/src/connect/DBConnection.java
+++ feed v2/litmus7/src/connect/DBConnection.java
@@ -1,17 +1,32 @@
 package connect;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String url = "jdbc:mysql://localhost:3306/inventorytable";
-    private static final String user = "root";
-    private static final String password = "root@123";
+    private static Connection connection = null;
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, password);
-    }
+        if (connection == null || connection.isClosed()) {
+            try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("prop.properties")) {
 
+                Properties props = new Properties();
+                props.load(input);
+
+                String url = props.getProperty("db.url");
+                String user = props.getProperty("db.username");
+                String password = props.getProperty("db.password");
+
+                connection = DriverManager.getConnection(url, user, password);
+
+            } catch (Exception e) {
+                throw new SQLException("Failed to load DB properties or establish connection", e);
+            }
+        }
+        return connection;
+    }
 }
